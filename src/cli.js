19be29ec6105c97f96a4ff826cf0b97bb2f5dbc7
imgyrdym <61790107+imgyrdym@users.noreply.github.com>
2024-02-02 12:12:34 +0700

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,8 @@ import readlineSync from 'readline-sync';
 const greeting1 = () => {
     // console.log('Welcome stranger!');
     console.log('Welcome to the Brain Games!')
-    const name = readlineSync.question('How do you call yourself? ');
+    // const name = readlineSync.question('How do you call yourself? ');
+    const name = readlineSync.question(' May I have your name?');
     console.log(`Hi ${name}!`);
 };
 
