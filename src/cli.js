9cec9b309b31b2771e6db0508ee89c35ed299143
imgyrdym <61790107+imgyrdym@users.noreply.github.com>
2024-02-02 12:15:29 +0700

    Update cli.js once mooooroe for autotesting

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,7 @@ const greeting1 = () => {
     console.log('Welcome to the Brain Games!')
     // const name = readlineSync.question('How do you call yourself? ');
     const name = readlineSync.question(' May I have your name?');
-    console.log(`Hi ${name}!`);
+    console.log(`Hello, ${name}!`);
 };
 
 export default greeting1()
